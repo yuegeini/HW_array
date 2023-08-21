51164db5493f92d1dcef8b2355a16fab705dda6f
Name <barmatine99@gmail.com>
2023-08-21 18:03:14 +0300

    Reformat code

--- src/main/java/ru/netology/stats/StatsServices.java
+++ src/main/java/ru/netology/stats/StatsServices.java
@@ -15,17 +15,18 @@ public class StatsServices {
 
     public int numOfTopSaleMonth(int[] arr) {
         int numOfMax = 0;
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] >= arr[numOfMax]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= arr[numOfMax]) {
                 numOfMax = i;
             }
         }
         return numOfMax + 1;
     }
+
     public int numOfBotSaleMonth(int[] arr) {
         int numOfMin = 0;
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] <= arr[numOfMin]) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= arr[numOfMin]) {
                 numOfMin = i;
             }
         }
@@ -35,16 +36,17 @@ public class StatsServices {
     public int countOfLowAverageSale(int[] arr) {
         int count = 0;
         double av = averageSum(arr);
-        for(int elm : arr) {
-            if(elm < av) count++;
+        for (int elm : arr) {
+            if (elm < av) count++;
         }
         return count;
     }
+
     public int countOfHighAverageSale(int[] arr) {
         int count = 0;
         double av = averageSum(arr);
-        for(int elm : arr) {
-            if(elm > av) count++;
+        for (int elm : arr) {
+            if (elm > av) count++;
         }
         return count;
     }
